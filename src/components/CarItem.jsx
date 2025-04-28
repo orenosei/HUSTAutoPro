001a83539773c8b0b1355f8eda04f465d10ca2a2
Minh Thanh Trinh <thanh.tm.dev@gmail.com>
2025-04-28 22:50:38 +0700

    readme

---
+++ src/components/CarItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CarItem() {
+  return (
+    <div>CarItem</div>
+  )
+}
+
+export default CarItem
---
+++ src/components/MostSearchedCar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MostSearchedCar() {
+  return (
+    <div>MostSearchedCar</div>
+  )
+}
+
+export default MostSearchedCar
