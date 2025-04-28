81f41fef496b6e49df4dd279b69367b81e663802
Minh Thanh Trinh <thanh.tm.dev@gmail.com>
2025-04-28 22:07:52 +0700

    update header

--- src/home.jsx
+++ src/home.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Button } from './components/ui/button'
+import { SignInButton } from '@clerk/clerk-react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Category from './components/Category'
+import MostSearchedCar from './components/MostSearchedCar'
+import InfoSection from './components/InfoSection'
+import Footer from './components/Footer'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      {/*<Header /> */}
+      <Header />
+
+      {/*<Hero /> */}
+      <Hero />
+
+      {/*<Category/> */}
+      <Category />
+
+      {/*<MostSearchedCar /> */}
+      <MostSearchedCar />
+
+      {/*<InfoSection /> */}
+      <InfoSection />
+
+      {/*<Footer /> */}
+      <Footer />
+
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter } from 'react-router-dom'
-import { Home } from 'lucide-react'
+import {  RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+import Home from './home'
 
 const router = createBrowserRouter([
   {
@@ -11,9 +13,18 @@ const router = createBrowserRouter([
   }
 ])
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+} 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <RouterProvider router={router}/> 
+    </ClerkProvider>
     
-    <RouterProvider router={router} /> 
+    
   </StrictMode>,
 )
