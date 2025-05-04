7d5c1efc6db42113a4fc8a88a85064d2c4142b0e
Minh Thanh Trinh <thanh.tm.dev@gmail.com>
2025-05-04 18:03:18 +0700

    update readme

--- drizzle/drizzle.config.js
+++ drizzle/drizzle.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./schema.ts",
+  schema: "./configs/schema.js",
   dbCredentials: {
     url:'postgresql://admin:npg_D7t9UAvKruTO@ep-sweet-sound-a1rrjll5-pooler.ap-southeast-1.aws.neon.tech/hust-auto-pro?sslmode=require'
   },
