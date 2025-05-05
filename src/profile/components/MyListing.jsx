8e9de76c1ff4bfe0a4552d40c69c0d637ab4b23f
Dinhvanphamviet <vietphamdinhvan.com>
2025-05-05 23:19:09 +0700

    viet11h18phut

--- src/profile/components/MyListing.jsx
+++ src/profile/components/MyListing.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
 import { Button } from '@/components/ui/Button'
+//import { useUser } from '@clerk/clerk-react'
+//import { db } from './../../../configs'
+//import { CarListing, CarImages } from './../../../configs/schema'
+//import { eq, desc } from 'drizzle-orm'
+import React, { useEffect } from 'react' 
+import { Link } from 'react-router-dom'
+//import { useState } from 'react'
+//import Service from '@/Shared/Service'
+
 function MyListing() {
+
+  // const {user}=useUser();
+  // const [carList,setCarList]=useState([]);
+
+  // useEffect(()=>{
+  //   user && GetUserCarListing();
+
+  // }, [user])
+  // const GetUserCarListing=async()=>{
+  //   const result=await db.select().from(CarListing)
+  //   .leftJoin(CarImages,eq(CarListing.id,CarImages.carListingId))
+  //   .where(eq(CarListing.createdBy,user?.primaryEmailAddress?.emailAddress))
+  //   .orderBy(desc(CarListing.id))
+
+  //   const resp=Service.FormatResult(result)
+  //   console.log(result);
+  //   setCarList(resp);
+  // }
+
+
   return (
 
     <div className='mt-6'>
@@ -11,8 +38,15 @@ function MyListing() {
                 <Button className='bg-blue-500 text-white hover:scale-110' > + Add New Listing</Button>
             </Link>
         </div>
-    </div>
+        <div>
+          {carList.map((item,index)=>(
+            <div key={index}>
+                <CarItem car={item} />
+              </div>
+          ))}
+        </div>
+    </div>  
   )
 }
 
-export default MyListing
+export default MyListing;
