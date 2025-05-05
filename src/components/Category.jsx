c1694e95db3e2e58fc1bdeda18a52fefd59cfe9b
Minh Thanh Trinh <thanh.tm.dev@gmail.com>
2025-05-05 11:46:23 +0700

    upload ho viet

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Data from "@/Shared/Data"
 
 function Category() {
   return (
-    <div>Category</div>
+    <div className='mt-40'>
+      <h2 className='font-bold text-3xl text-center mb-6'> Browse By Type</h2>
+
+      <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-9 gap-6 px-20'>
+        {Data.Category.map((category,index) => (
+          <div className='border rounded-xl p-3 items-center flex flex-col hover:shadow-md cursor-pointer'>
+            <img src={category.icon} width={40} height={40} />
+            <h2 className=''>{category.name}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
