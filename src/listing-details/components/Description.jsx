e7ed074937c0eaa5b06e4174465d57a20b0e4b24
Minh Thanh Trinh <thanh.tm.dev@gmail.com>
2025-05-11 00:12:12 +0700

    update listing detail

---
+++ src/listing-details/components/Description.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Description({carDetail}) {
+  return (
+    <div>
+    {carDetail?.listingDescription? 
+    <div className='p-10 rounded-xl bg-white shadow-md mt-6 border border-gray-200 '>
+        <h2 className='text-2xl font-semibold'>Description</h2>
+        <p className='text-gray-600  text-justify mt-10'>{carDetail?.listingDescription}</p>
+    </div>
+    :
+    <div className='w-full mt-7 h-[100px] bg-slate-200 animate-pulse rounded-xl'></div>
+    }
+    </div>
+  )
+}
+
+export default Description
