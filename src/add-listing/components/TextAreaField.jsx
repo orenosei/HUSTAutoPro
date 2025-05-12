935fde1041a82e2db1a01b06ee259c066158e0e7
Nguyễn Hải Dương <nhd0219@gmail.com>
2025-05-12 16:20:16 +0700

    abc

--- src/add-listing/components/TextAreaField.jsx
+++ src/add-listing/components/TextAreaField.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Textarea} from "@/components/ui/textarea"
 
-function TextAreaField({item,handleInputChange}) {
+function TextAreaField({item,handleInputChange,carInfo}) {
   return (
     <div>
       <Textarea onChange={(e)=>handleInputChange(item.name,e.target.value)}
-      required={item.required} />
+      required={item.required} 
+      defaultValue={carInfo?.[item.name]}/>
     </div>
   )
 }
