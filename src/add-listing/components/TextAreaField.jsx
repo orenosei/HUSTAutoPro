7ee6463b70835d9a979139eb366c5031839b0e1f
Minh Thanh Trinh <thanh.tm.dev@gmail.com>
2025-05-21 06:31:30 +0700

    peakkkkkk

--- src/add-listing/components/TextAreaField.jsx
+++ src/add-listing/components/TextAreaField.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import {Textarea} from "@/components/ui/textarea"
+import { useState, useEffect } from "react";
 
-function TextAreaField({item,handleInputChange,carInfo}) {
+function TextAreaField({ item, handleInputChange, carInfo }) {
+  const [value, setValue] = useState(carInfo?.[item.name] || '');
+
+  useEffect(() => {
+    setValue(carInfo?.[item.name] || '');
+  }, [carInfo?.[item.name]]);
+
   return (
-    <div>
-      <Textarea className="w-full px-4 py-3 border-gray-300 rounded-lg focus:ring-2 focus:ring-red-500 group-hover:border-red-300 transition-colors" onChange={(e)=>handleInputChange(item.name,e.target.value)}
-      required={item.required} 
-      defaultValue={carInfo?.[item.name]}/>
-    </div>
-  )
+    <Textarea
+    className="px-4 py-3 border-gray-300 rounded-lg focus:ring-2 focus:ring-red-500 focus:border-red-500"
+      value={value}
+      onChange={(e) => {
+        setValue(e.target.value);
+        handleInputChange(item.name, e.target.value);
+      }}
+    />
+  );
 }
 
 export default TextAreaField
