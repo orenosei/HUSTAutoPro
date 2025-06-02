30626abc1a35e8c8741ac20fc56e2d3f3b0004b1
llong1509 <longzzhd@gmail.com>
2025-06-02 16:23:50 +0700

    fix

--- src/profile/index.jsx
+++ src/profile/index.jsx
@@ -8,6 +8,7 @@ import MyBlog from './components/MyBlog'
 import { useUser } from '@clerk/clerk-react'
 import Service from '@/Shared/Service'
 import { useState, useEffect } from 'react'
+import ViewHistory from './components/ViewHistory'
 
 
 function Profile() {
@@ -71,6 +72,10 @@ function Profile() {
 
         <TabsContent value="favorite">
         <FavoriteList />
+        </TabsContent>
+
+        <TabsContent value="history">
+          <ViewHistory currentUserId={currentUser?.id} />
         </TabsContent>
         </Tabs>
     </div>
