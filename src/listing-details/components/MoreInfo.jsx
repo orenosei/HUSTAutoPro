e99d9058c3336a61efd33cd75cd24f6d68604675
Minh Thanh Trinh <thanh.tm.dev@gmail.com>
2025-06-04 22:17:22 +0700

    done arya

--- src/listing-details/components/MoreInfo.jsx
+++ src/listing-details/components/MoreInfo.jsx
@@ -25,7 +25,7 @@ const MoreInfo = ({ carDetail }) => {
       <img src="/arya.png" alt="Arya Avatar" className="h-30 w-30 " />
       <button
         onClick={handleAskAboutCar}
-        className="w-full bg-gradient-to-r from-pink-600 to-blue-500 text-white py-3 rounded-lg hover:from-pink-700 hover:to-blue-600 transition-all flex items-center justify-center"
+        className="w-full text-xl bg-gradient-to-r from-pink-600 to-blue-500 text-white py-3 rounded-lg hover:from-pink-700 hover:to-blue-600 transition-all flex items-center justify-center hover:scale-105 focus:outline-none focus:ring-2 focus:ring-pink-300 focus:ring-offset-2"
       >
         Trao đổi ngay với Arya !
       </button>
